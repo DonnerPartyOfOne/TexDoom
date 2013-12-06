fb345cf7f8e127bb3fd244c0ffd4f77c66c96a08
danny <danny@the-craptop.(none)>
2013-12-06 17:15:47 -0500

    Began class templates for characters and weapons

---
+++ Character.h
@@ -0,0 +1,13 @@
+class Character {
+	public:
+		int getAgility();
+		int getAccuracy();
+		int getMelee();
+		int getHitpoints();
+
+	private:
+		int agility;
+		int accuracy;
+		int melee;
+		int hitpoints;
+}
---
+++ Weapon.h
@@ -0,0 +1,13 @@
+class Weapon {
+	public:
+		int getWeaponAccuracy();
+		int getFireRate();
+		int getRange();
+		int getMagazineCapacity();
+
+	private:
+		int weaponAccuracy;
+		int fireRate;
+		int range;
+		int magazineCapacity;
+}
